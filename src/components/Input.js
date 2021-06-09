e9b8c24eae38fab1de2c8cdf154dba5b0f6ff677
Ben <benjaminomitto@gmail.com>
2021-06-10 00:25:04 +0300

    init

---
+++ src/components/Input.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Formik,Form} from 'formik'
+import * as Yup from 'yup'
+import FormikControls from './FormikControls'
+function FormikContainer(){
+	const initialValues = {email:''}
+	const validationSchema = Yup.object({email:Yup.string().required('Required')
+	})
+	const onSubmit = values => console.log('Form data',values);
+	
+	return(
+	
+		<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onsubmit}>
+			{
+				formik => 
+				
+				<Form>
+					<FormikControls control='input' type='email' label='Email' name='email'/>
+					<button type='submit'>Submit</button>
+				</Form>
+			}
+		</Formik>
+		
+		)
+}
+
+export default FormikContainer
---
+++ src/components/TextError.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TextError(){
+	
+
+	return(
+	
+	<div className='error'>
+		{props.children}
+	</div>
+	
+	)
+}
+
+export default TextError
